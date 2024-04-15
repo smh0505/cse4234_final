fbe7513523951545bbb9690fb0cf4edd807b0b78
Bloppy <mson9705@gmail.com>
2024-04-14 22:20:20 -0400

    Error in recipes.json fixed

--- client/src/RecipeCard.jsx
+++ client/src/RecipeCard.jsx
@@ -1,24 +1,30 @@
 import "./RecipeCard.css";
 
-const regex = /PT(?:([.,\d]+)H)?(?:([.,\d]+)M)?/;
+const regex = /P(?:([.,\d]+)D)?T?(?:([.,\d]+)H)?(?:([.,\d]+)M)?/;
 
 export default function RecipeCard(props) {
   const getTime = (time) => {
-    let duration = time || "N/A";
-    if (duration !== "N/A") {
-      const matches = time.match(regex);
-      duration =
-        String(matches[1] || 0).padStart(2, "0") +
-        ":" +
-        String(matches[2] || 0).padStart(2, "0");
-    }
-    return duration;
+    if (!time) return "N/A";
+
+    const matches = time.match(regex);
+
+    let hour =
+      (matches[1] ? Number(matches[1]) : 0) * 24 +
+      (matches[2] ? Number(matches[2]) : 0);
+    let minute = matches[3] ? Number(matches[3]) : 0;
+
+    hour += Math.floor(minute / 60);
+    minute %= 60;
+
+    return (
+      String(hour).padStart(2, "0") + ":" + String(minute).padStart(2, "0")
+    );
   };
 
   return (
     <>
       <article>
-        <img src={props.recipe.image} loading="lazy"/>
+        <img src={props.recipe.image} loading="lazy" />
         <h3>{props.recipe.name}</h3>
         <p>
           <strong>Cook Time</strong>: {getTime(props.recipe.cookTime)} <br />
